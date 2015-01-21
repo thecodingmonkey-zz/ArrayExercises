ab574c7d319d95d42dad1cbe1484fd2aa786cc60
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-20 16:52:04 -1000

    level 1 finished

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,54 @@
  **/
 
 // Create an empty array.
-
+var empty_array = [];
 
 // Create an array of the colors of the rainbow.
-
+var rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'indigo'];
 
 // Create an array of gems.
-
+var gems = ['opal', 'ruby', 'sapphire', 'onyx', 'emerald', 'diamond'];
 
 // Create an array of military ranks.
-
+var military_ranks = ['sargeant', 'colonel', 'commander'];
 
 // Create an array of road signs.
-
+var road_signs = ['merge', 'speed limit', 'stop', 'call box'];
 
 // Create an array containing components of a tree.
-
+var tree = ['branch', 'leaf', 'trunk'];
 
 // Create an array of prime numbers less than 20.
-
+var primes = [2, 3, 5, 7, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
-
+var programmerWords = ['crap', 'f*ck', 'sh*t', 'damnit'];
 
 // Create an array of elements of the periodic table.
-
+var periodicTable = ['hydrogen', 'helium', 'lithium', 'carbon', 'neon'];
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = [25, 25];
 
 // Create an array of items you bring to class daily.
-
+var classItems = ['computer', 'jacket', 'keycard'];
 
 // Create an array of web developer role titles.
-
+var webDevRoles = ['UX Designer', 'UI Designer', 'UI Developer', 'JS Developer', 'Backend Developer'];
 
 // Create an array of good luck charms.
-
+var goodLuckCharms = ['penny', 'four-leaf clover', 8];
 
 // Create an array of mythical animals.
-
+var mythicalAnimals = ['unicorn', 'dragon', 'pegasus'];
 
 // Create an array of drinks.
-
+var drinks = ['coke', 'pepsi', 'water', 'fizzy water'];
 
 // Create an array of emoticons.
-
+var emoticons = [':)', ':/', 'o.o','=^.^='];
 
 // Create an array with the letters of the word 'array'.
-
+var array_letters = ['a', 'r', 'r', 'a', 'y'];
 
 
