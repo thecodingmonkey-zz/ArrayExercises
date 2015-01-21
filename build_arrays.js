dfa20e212d80936670f1e3dccbcace0638a2b2cf
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-20 17:04:51 -1000

    finish level 2

--- build_arrays.js
+++ build_arrays.js
@@ -61,21 +61,72 @@ var array_letters = ['a', 'r', 'r', 'a', 'y'];
 
 // Create an array of Objects that represent Famous People.
 // Each Object should have a Name, Age, and their Profession as properties
+var famous_people = [
+  {
+    name: "George Washington",
+    age: 42,
+    profession: "president"
+  },
+  {
+    name: "Alan Turing",
+    age: 28,
+    profession: "mathematician"
+  },
+  {
+    name: "Linda Lingle",
+    age: 60,
+    profession: "governor"
+  },
 
+];
 
 // Create an array of songs you'd like Jon to add to the playlist of music 
 // to listen to in class.  Each song should be an object with at least 
 // a title and an artist.
-
+var playlist = [
+  {
+    title: 'MAX300',
+    artist: 'Ω'
+  },
+  {
+    title: 'Let It Go',
+    artist: 'Idina Menzel'
+  }
+];
 
 // Create an array of functions. Define any functions you want, they can 
 // be anything! But make sure the syntax is correct :D
-
+var functions = [
+  function() {
+    return 0;
+  },
+  function(a, b, c) {
+    return a*b+c;
+  },
+  function() {
+    return arguments[1] + arguments[0];
+  }
+];
 
 // Create an array of Sublime shortcuts.  Each item should have a keystroke 
 // combination and a textual description of what the key combination does. 
 // You can find Sublime Text Shortcuts by traversing the Sublime Text's GUI
 // e.g. File Menu options.
+var sublimeShortcuts = [
+  {
+    keystroke: 'shift+command+P',
+    description: 'pellet command'
+  },
+  {
+    keystroke: 'shift+command+D',
+    description: 'duplicate line or selection'
+  },
+  {
+    keystroke: 'command+S',
+    description: 'save file'
+  }
+];
+
 
 
 
